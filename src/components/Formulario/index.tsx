5697bf0fc42d138e38bb9129c9364427c074d428
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-25 16:19:04 -0300

    Criando eventos com hooks

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { IEvento } from '../../interfaces/IEvento';
 import style from './Formulario.module.scss';
+import { obterId } from '../../util';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../state/atom';
 
-const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalvar }) => {
+const Formulario: React.FC = () => {
+
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+
   const [descricao, setDescricao] = useState('')
   const [dataInicio, setDataInicio] = useState('')
   const [horaInicio, setHoraInicio] = useState('')
@@ -16,12 +22,14 @@ const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalva
 
   const submeterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    aoSalvar({
+    const evento = {
+      id: obterId(),
       descricao,
       inicio: montarData(dataInicio, horaInicio),
       fim: montarData(dataFim, horaFim),
       completo: false
-    })
+    }
+    setListaDeEventos(listaAntiga => [...listaAntiga, evento])
     setDescricao('')
     setDataInicio('')
     setHoraInicio('')
