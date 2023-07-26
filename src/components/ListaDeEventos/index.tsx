64f2cd56ea19a2bd44627a3ea35aac6bf3d993d0
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-26 16:51:15 -0300

    Filtrando com seletores

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -3,23 +3,11 @@ import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 import useListaDeEventos from '../../state/hooks/useListaDeEventos';
-import { useRecoilValue } from 'recoil';
-import { IFiltroDeEventos } from '../../interfaces/IFiltroDeEventos';
-import { filtroDeEventos } from '../../state/atom';
 
 const ListaDeEventos: React.FC = () => {
 
-  const todosOsEventos = useListaDeEventos();
-  const filtro = useRecoilValue<IFiltroDeEventos>(filtroDeEventos)
+  const eventos = useListaDeEventos();
 
-  const eventos = todosOsEventos.filter(evento => {
-    if (!filtro.data) {
-      return true
-    }
-    const ehOMesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-    return ehOMesmoDia
-  })
-  
   return (<section>
     <Filtro />
     <div className={style.Scroll}>
--- src/state/hooks/useListaDeEventos.ts
+++ src/state/hooks/useListaDeEventos.ts
@@ -1,8 +1,8 @@
 import { useRecoilValue } from "recoil"
-import { listaDeEventosState } from "../atom"
+import { eventosFiltradosState } from "../seletores"
 
 const useListaDeEventos = () => {
-    return useRecoilValue(listaDeEventosState)
+    return useRecoilValue(eventosFiltradosState)
 }
 
 export default useListaDeEventos
