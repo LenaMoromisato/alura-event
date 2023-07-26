0c415579cf2a169afcff7b8e011ee8f5c214bd17
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-26 16:06:45 -0300

    Um átomo para a todos filtrar

--- src/components/Filtro/index.tsx
+++ src/components/Filtro/index.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import style from './Filtro.module.scss';
+import { useSetRecoilState } from 'recoil';
+import { IFiltroDeEventos } from '../../interfaces/IFiltroDeEventos';
+import { filtroDeEventos } from '../../state/atom';
 
-const Filtro: React.FC<{ aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
+const Filtro: React.FC = () => {
   
   const [data, setData] = useState('')
+  const setFiltroDeEvento = useSetRecoilState<IFiltroDeEventos>(filtroDeEventos)
   
   const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault()
-    if (!data) {
-      aoFiltroAplicado(null)
-      return
+    const filtro: IFiltroDeEventos = {}
+    if (data) {
+      filtro.data = new Date(data)
+    } else {
+      filtro.data = null
     }
-    aoFiltroAplicado(new Date(data))
+    setFiltroDeEvento(filtro)
   }
 
   return (<form className={style.Filtro} onSubmit={submeterForm}>
--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,5 +1,6 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
+import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listaDeEventosState',
@@ -19,4 +20,9 @@ export const listaDeEventosState = atom<IEvento[]>({
             "id": 1642342959
         }
     ]
+})
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+    key: 'filtroDeEventos',
+    default: {}
 })
