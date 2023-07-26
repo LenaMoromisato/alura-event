5ab0886b014474ad1af31a238b7636e7aae41723
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-26 15:41:51 -0300

    Lista de eventos via hook

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -4,9 +4,8 @@ import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
 import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
 import useAtualizarEvento from '../../state/hooks/useAtualizarEvento';
+import useListaDeEventos from '../../state/hooks/useListaDeEventos';
 
 interface IKalendEvento {
   id?: number
@@ -19,7 +18,7 @@ interface IKalendEvento {
 const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
-  const eventos = useRecoilValue(listaDeEventosState);
+  const eventos = useListaDeEventos();
   const atualizarEvento = useAtualizarEvento()
 
   eventos.forEach(evento => {
