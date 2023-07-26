32dac291d4e62f1aa2ca956ec1c97ca65f55f702
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-26 15:35:06 -0300

    Desafio: lidando com as exclusões

--- src/components/Evento/index.tsx
+++ src/components/Evento/index.tsx
@@ -4,14 +4,13 @@ import style from './Evento.module.scss';
 import EventoCheckbox from './EventoCheckbox';
 import { useSetRecoilState } from 'recoil';
 import { listaDeEventosState } from '../../state/atom';
+import useDeletarEvento from '../../state/hooks/useDeletarEvento';
 
 const Evento: React.FC<{ evento: IEvento }> = ({ evento }) => {
 
   const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
   
-  const excluirEvento = () => {
-    setListaDeEventos(listaAntiga => listaAntiga.filter(evt => evt.id !== evento.id))
-  }
+  const excluirEvento = useDeletarEvento()
   
   const estilos = [
     style.Evento
@@ -27,7 +26,7 @@ const Evento: React.FC<{ evento: IEvento }> = ({ evento }) => {
     <div className="cards-info">
       <h3 className={style.descricao}>{evento.descricao} - {evento.inicio.toLocaleDateString()}</h3>
     </div>
-    <i className="far fa-times-circle fa-2x" onClick={excluirEvento}></i>
+    <i className="far fa-times-circle fa-2x" onClick={() => excluirEvento(evento)}></i>
   </div>)
 }
 
