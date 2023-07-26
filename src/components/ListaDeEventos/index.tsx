f101eb327a483449acb6cac23ceaaba0e92a85e5
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-26 16:38:58 -0300

    Aplicando o filtro

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -3,10 +3,22 @@ import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 import useListaDeEventos from '../../state/hooks/useListaDeEventos';
+import { useRecoilValue } from 'recoil';
+import { IFiltroDeEventos } from '../../interfaces/IFiltroDeEventos';
+import { filtroDeEventos } from '../../state/atom';
 
 const ListaDeEventos: React.FC = () => {
 
-  const eventos = useListaDeEventos();
+  const todosOsEventos = useListaDeEventos();
+  const filtro = useRecoilValue<IFiltroDeEventos>(filtroDeEventos)
+
+  const eventos = todosOsEventos.filter(evento => {
+    if (!filtro.data) {
+      return true
+    }
+    const ehOMesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
+    return ehOMesmoDia
+  })
   
   return (<section>
     <Filtro />
