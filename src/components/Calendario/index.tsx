26567d200f89183e96d5871976d553ecc01450c4
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-26 10:58:16 -0300

    Corrigindo o bug

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -2,10 +2,11 @@
 import React from 'react'
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { listaDeEventosState } from '../../state/atom';
+import { IEvento } from '../../interfaces/IEvento';
 
 interface IKalendEvento {
   id?: number
@@ -19,6 +20,7 @@ const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
   const eventos = useRecoilValue(listaDeEventosState);
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -33,6 +35,26 @@ const Calendario: React.FC = () => {
       color: 'blue'
     })
   })
+
+  const onEventDragFinish: OnEventDragFinish = (
+    kalendEventoInalterado: CalendarEvent,
+    kalendEventoAtualizado: CalendarEvent
+  ) => {
+    const evento = eventos.find(evento => evento.descricao === kalendEventoAtualizado.summary)
+    if (evento) {
+      const eventoAtualizado = {
+        ...evento
+      }
+      eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt)
+      eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt) 
+
+      setListaDeEventos(listaAntiga => {
+        const indice = listaAntiga.findIndex(evt => evt.id === evento.id)
+        return [...listaAntiga.slice(0, indice), eventoAtualizado, ...listaAntiga.slice(indice + 1)]
+      })
+    }
+  }
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -45,6 +67,7 @@ const Calendario: React.FC = () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
+        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
