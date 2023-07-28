19ddf2d6bdd2e275de62467140996c30a891b905
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-28 16:09:14 -0300

    Observando alterações no estado

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Calendario from './components/Calendario';
 import ListaDeEventos from './components/ListaDeEventos';
 import { RecoilRoot } from 'recoil';
 import { Suspense } from 'react';
+import DebugObserver from './components/DebugObserver';
 
 function App() {
   return (
     <RecoilRoot>
+      <DebugObserver />
       <Suspense fallback="Está carregando">
         <div className={style.App}>
           <div className={style.Coluna}>
